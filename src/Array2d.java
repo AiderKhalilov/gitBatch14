bb89557a3c5e78305c4489d8c1ed7a7cfdf5522b
AiderKhalilov <113560876+AiderKhalilov@users.noreply.github.com>
2022-09-28 19:12:03 -0400

    deleted 4th raw

--- src/Array2d.java
+++ src/Array2d.java
@@ -5,7 +5,7 @@ public class Array2d {
                 {1.4, 2.0, 3.3, 2.0},
                 {4.0, 1.5, 6.1, 1.0},
                 {1.2, 3.1, 4.0, 1.6},
-                {2.2,3.4},
+                
         };
         for(int r=0; r<num.length; r++){
             for(int c=0; c<num.length; c++){
